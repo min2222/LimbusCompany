dc91744edcca65b27a584a2e010e80f3f36a9456
min01 <goyoumin2@gmail.com>
2025-09-03 11:34:42 +0900

    Update OverheatEffect.java

--- src/main/java/com/min01/limbus/effect/OverheatEffect.java
+++ src/main/java/com/min01/limbus/effect/OverheatEffect.java
@@ -15,6 +15,7 @@ public class OverheatEffect extends MobEffect
 		this.addAttributeModifier(Attributes.ATTACK_DAMAGE, UUID.randomUUID().toString(), -4.0F, Operation.ADDITION);
 		this.addAttributeModifier(Attributes.ATTACK_SPEED, UUID.randomUUID().toString(), 0.8F, Operation.ADDITION);
 		this.addAttributeModifier(Attributes.ATTACK_SPEED, UUID.randomUUID().toString(), 20.0F / 100.0F, Operation.MULTIPLY_BASE);
+		this.addAttributeModifier(Attributes.MOVEMENT_SPEED, UUID.randomUUID().toString(), 10.0F / 100.0F, Operation.MULTIPLY_BASE);
 	}
 	
 	@Override
